8d58b55acda074cf16213beb5e7a1eb26043b177
akash kumar <akash639673@gmail.com>
2026-07-08 04:36:19 +0530

    redis2

--- backend/src/services/crawler/crawlerService.js
+++ backend/src/services/crawler/crawlerService.js
@@ -7,11 +7,14 @@ class CrawlerService {
     const startTime = Date.now();
 
     try {
-      browser = await puppeteer.launch({
-        headless: "new",
-
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      });
+      const browser = await puppeteer.launch({
+  headless: true,
+  args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+  ],
+});
 
       const page = await browser.newPage();
 
