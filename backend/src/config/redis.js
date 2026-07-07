c160e5b2a9f446800fa0a615b85e7f0644b54d69
akash kumar <akash639673@gmail.com>
2026-07-08 04:28:36 +0530

    redis

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -1,8 +1,14 @@
 const Redis = require("ioredis");
 
+console.log("HOST:", process.env.REDIS_HOST);
+console.log("PORT:", process.env.REDIS_PORT);
+console.log("PASSWORD:", process.env.REDIS_PASSWORD ? "FOUND" : "NOT FOUND");
+
 const redis = new Redis({
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+  tls: {},
   maxRetriesPerRequest: null,
 });
 
